7f1677629d67569abada19d43d7d06da2cc4f937
Winsaid <147753007+Winsaid@users.noreply.github.com>
2024-11-20 23:56:18 +0300

    the revival of the project

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,41 +1,9 @@
-﻿// main.cpp: определяет точку входа для приложения.
-//
+﻿#include "../header/Main.h"
 
-#include <SFML/Graphics.hpp>
+using namespace std;
 
 int main()
 {
-
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-
-	sf::CircleShape shape(100.f);
-
-	shape.setFillColor(sf::Color::Green);
-
-	while (window.isOpen())
-
-	{
-
-		sf::Event event;
-
-		while (window.pollEvent(event))
-
-		{
-
-			if (event.type == sf::Event::Closed)
-
-				window.close();
-
-		}
-
-		window.clear();
-
-		window.draw(shape);
-
-		window.display();
-
-	}
-
+	cout << "Hello CMake." << endl;
 	return 0;
-
 }
